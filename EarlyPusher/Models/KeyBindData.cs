af817b0ac51ea085f45362ba7060205548e52784
Freesia <freesia@studiofreesia.com>
2014-12-05 00:11:16 +0000

    色データ追加
    git-svn-id: https://svn.studiofreesia.com:81/svn/StudioFreesiaApps/EarlyPusher@46 7b666dab-8978-da46-a52b-f1a99afe411e

--- EarlyPusher/Models/KeyBindData.cs
+++ EarlyPusher/Models/KeyBindData.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Media;
 
 namespace EarlyPusher.Models
 {
 	public class KeyBindData : ObservableObject
 	{
 		private Guid deviceGuid;
+		private int key;
 
 		public Guid DeviceGuid
 		{
@@ -16,12 +18,19 @@ namespace EarlyPusher.Models
 			set { SetProperty( ref deviceGuid, value ); }
 		}
 
-		private int key;
-
 		public int Key
 		{
 			get { return key; }
 			set { SetProperty( ref key, value ); }
 		}
+
+		private Color panelColor;
+
+		public Color PanelColor
+		{
+			get { return panelColor; }
+			set { SetProperty( ref panelColor, value ); }
+		}
+
 	}
 }
